4e7f8e2b381f0de5d2bc3cef669384ecb4724da1
toblorono <84089176+toblorono@users.noreply.github.com>
2021-08-09 08:00:37 -0500

    Update witness.py
    Previously had the same functionality as deprave.py

--- cults_depravity/witness.py
+++ cults_depravity/witness.py
@@ -1,25 +1,29 @@
 embed
 <drac2>
-args, c, n, f, fT = argparse(&ARGS&), combat(), "\n", "", ""
+args, c, n, f = argparse(&ARGS&), combat(), "\n", ""
 
 combatants = args.get("t")
 
 for targ in combatants:
-	if cb := c.get_combatant(targ):
-		if e := cb.get_effect("Cult's Depravity"):
-			stacks = int(e.name[18:])
-			r = vroll(f"{stacks}d4[psychic]")
-			cb.damage(r.total)
-			f = f + f""" -f "{cb.name}|**Damage:** {r.result}" """
-			fT = fT + f"""{cb.name}: {cb.hp_str()}{n}"""
-		else:
-			f = f + f""" -f "{cb.name}|No stacks of Cult's Depravity found!" """
-	else:
-		fT = fT + f"""{targ}: Combatant not found{n}"""
-</drac2>
+    if cb := c.get_combatant(targ):
+        if e := cb.get_effect("Cult's Depravity"):
+            stacks = int(e.name[18:])+1
+            cb.remove_effect("Cult's Depravity")
+        else:
+            stacks = 1
+        if "clear" not in args:
+            cb.add_effect(f"Cult's Depravity: {stacks}", "")
+            adj = "Witnessed"
+        else:
+            stacks = "Absolved"
+            adj = "Shaken"
+        f = f + f"""-f "{cb.name}|Cult's Depravity: {stacks}{n}" """
+    else:
+        f = f + f""" -f "{targ}|Combatant not found." """
 
--title "The Flesh Mound's Depravity is Inflicted!" 
--desc "Any target afflicted by the Cult's Depravity takes 1d4 psychic damage at the start of its turn, which increases by 1d4 damage per stack accumulated. An afflicted target can use their action to shake these visions entirely and eliminate all existing stacks of the Cult's Depravity." 
+</drac2>
+-title "The Flesh Mound's Depravity is {{adj}}!" 
+-desc "Any creature within 60 feet that can see the Flesh Mound must make a DC 14 intelligence Saving throw or take 7 (2d6) psychic damage and gain 1 stack of the Cult's Depravity as it is assailed with horrible visions of the cult's sacrifices in the ritual chamber. The target takes half damage and is not afflicted by a Cult's Depravity stack on a successful save. {{n}}{{n}}Any target afflicted by the Cult's Depravity takes 1d4 psychic damage at the start of its turn, which increases by 1d4 damage per stack accumulated. An afflicted target can use their action to shake these visions entirely and eliminate all existing stacks of the Cult's Depravity." 
 -thumb <image> 
 {{f}} 
--footer "{{fT}} Usage: !deprave -t [targ1] -t [targ2] ... -t [targN]"
+-footer "Usage: !witness [clear] -t [targ1] -t [targ2] ... -t [targN]"
